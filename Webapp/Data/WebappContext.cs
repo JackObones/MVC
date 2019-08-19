c0aab9cd61292d70a67aeb98ae8d8569cb459d68
Gustavo <gdragnell@live.com>
2019-08-19 15:33:59 -0300

    Crud scaffould

---
+++ Webapp/Data/WebappContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Webapp.Models
+{
+    public class WebappContext : DbContext
+    {
+        public WebappContext (DbContextOptions<WebappContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Webapp.Models.Department> Department { get; set; }
+    }
+}
--- Webapp/Startup.cs
+++ Webapp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Webapp.Models;
 
 namespace Webapp
 {
@@ -33,6 +35,9 @@ namespace Webapp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebappContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebappContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
